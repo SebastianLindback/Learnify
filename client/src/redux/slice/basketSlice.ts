5c41677cb99c9ec09c14e8f18e9d6925492b0fd3
Sebastian <sebbe.lindback@hotmail.com>
2022-09-25 18:12:51 +0200

    Modul 9. Part 6: Using Asynchronous Calls in Redux

--- client/src/redux/slice/basketSlice.ts
+++ client/src/redux/slice/basketSlice.ts
@@ -1,13 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import agent from "../../actions/agent";
 import { Basket } from "../../models/basket";
 
 interface BasketState{
-    basket: Basket | null;
+    basket: Basket | null | undefined;
+    status: string;
 }
 
 const initialState: BasketState = {
     basket : null,
+    status: "idle",
 }
+export const addBasketItemAsync = createAsyncThunk<Basket | undefined, {courseId : string}>(
+    "baket/addBasketItemAsync",
+   async ({courseId}) => {
+    try {
+        return await agent.Baskets.addItem(courseId);
+    } catch (error) {
+        console.log(error);
+    }
+   }
+);
 
 export const basketSlice = createSlice({
     name : "basket",
@@ -25,6 +38,18 @@ export const basketSlice = createSlice({
             state.basket?.items.splice(ItemIndex, 1); 
 
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(addBasketItemAsync.pending, (state) => {
+            state.status = "pending"
+        });
+        builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+            state.basket = action.payload;
+            state.status = "idle";
+        });
+        builder.addCase(addBasketItemAsync.rejected, (state) => {
+            state.status = "idle";
+        });
     },
 });
 
