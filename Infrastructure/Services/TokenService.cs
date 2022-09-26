6963d04856fc02eab7a8f6927f918e863809c3b8
Sebastian <sebbe.lindback@hotmail.com>
2022-09-26 17:25:02 +0200

    Module 10 Part 5-6: Creating Token Service

---
+++ Infrastructure/Services/TokenService.cs
@@ -0,0 +1,54 @@
+using System.Security.Claims;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Infrastructure.Services
+{
+    public class TokenService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _config;
+        public TokenService(UserManager<User> userManager, IConfiguration config)
+        {
+            _config = config;
+            _userManager = userManager;
+
+        }
+
+        public async Task<string> GenerateToken(User user)
+        {
+            var claims = new List<Claim>{
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.UserName),
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenOptions = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims,
+                expires: DateTime.Now.AddDays(10),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+    }
+}
