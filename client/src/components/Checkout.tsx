9515887b174a85582166728a43baedfb124ed93d
Sebastian <sebbe.lindback@hotmail.com>
2022-09-27 22:10:21 +0200

    Module 11. Part 7: Making Payment Function

--- client/src/components/Checkout.tsx
+++ client/src/components/Checkout.tsx
@@ -2,17 +2,60 @@ import {
     CardCvcElement,
     CardExpiryElement,
     CardNumberElement,
+    useElements,
+    useStripe,
   } from "@stripe/react-stripe-js";
-  import { Card, Form, Input } from "antd";
-  import { ChangeEvent, useState } from "react";
+  import { Card, Form, Input, message, notification } from "antd";
+  import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { useNavigate } from "react-router";
+import { removeBasket } from "../redux/slice/basketSlice";
+import { useAppDispatch, useAppSelector } from "../redux/store/ConfigureStore";
   import CheckoutSummary from "./CheckoutSummary";
   
   const Checkout = () => {
     const [cardName, setCardName] = useState < string > ("");
-  
+    const {basket} = useAppSelector((state) => state.basket);
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const stripe = useStripe();
+    const elements = useElements();
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
       setCardName(e.target.value);
     };
+
+    const handlePayment = async (event: SyntheticEvent) => {
+      event.preventDefault();
+      if (!stripe || !elements) return;
+      try {
+        const cardElement = elements.getElement(CardNumberElement);
+        const paymentResult = await stripe.confirmCardPayment(basket?.ClientSecret!,{ 
+          payment_method: {
+            card: cardElement!,
+            billing_details: {
+              name: cardName
+            }
+          }
+        });
+
+        if(paymentResult.paymentIntent?.status === "succeeded"){
+          notification.success({
+            message: "Your payment is successfull",
+          });
+          dispatch(removeBasket());
+          setTimeout(() => {
+            navigate("/profile");
+          }, 1000);
+        }
+        else {
+          notification.success({
+            message: paymentResult.error?.message!,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
   
     const [form] = Form.useForm();
   
