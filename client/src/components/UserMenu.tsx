d8a42fd7593d0fab3a7d03d75d73bc4d4308d82a
SebastianLindback <sebbe.lindback@hotmail.com>
2022-09-30 15:56:29 +0200

    Module 13 Part 1: Adding Instructor Role

--- client/src/components/UserMenu.tsx
+++ client/src/components/UserMenu.tsx
@@ -4,10 +4,14 @@ import { useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { removeBasket } from '../redux/slice/basketSlice';
 import { signOut } from '../redux/slice/userSlice';
+import { useAppSelector } from '../redux/store/ConfigureStore';
 
 function UserMenu() {
     const dispatch = useDispatch();
     const navigate = useNavigate()
+
+    const { user } = useAppSelector((state) => state.user);
+
     const signout = () => {
         dispatch(signOut());
         dispatch(removeBasket());
@@ -18,6 +22,11 @@ function UserMenu() {
             <Menu.Item>
                 <Link to="/profile">Profile</Link>
             </Menu.Item>
+            {user?.roles?.includes('Instructor') && (
+                <Menu.Item>
+                <Link to="/instructor">Instructor</Link>
+                </Menu.Item>
+            )}
             <Menu.Item>
                 <div onClick={signout}>Logout</div>
             </Menu.Item>
